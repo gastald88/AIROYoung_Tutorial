5248e3a2136e0dc21cf5ba6700451c33d700ba96
PC_FISSO_HOME\Gasta <gastald88@gmail.com>
2019-10-13 12:36:27 +0200

    setting up exercise and exercise solved

--- AYT_wd/Exercise_Solved/build/build_all.sh
+++ AYT_wd/Exercise_Solved/build/build_all.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cmake -DCMAKE_BUILD_TYPE=Release CMakeLists.txt
-cmake --build .
-cp lib/libAYT_exercise.so ../AYT_exercise.so
+cmake -DCMAKE_BUILD_TYPE=Release -Dboost_path="C:/Users/Gasta/Documents/Visual Studio 2017/Librerie/boost_1_68_0/" CMakeLists.txt
+cmake --build . --target install
+cp bin/Release/AYT_tutorial.so ../
+#cmake -DCMAKE_BUILD_TYPE=Debug -Dboost_path="C:/Users/Gasta/Documents/Visual Studio 2017/Librerie/boost_1_68_0/" CMakeLists.txt
+#cmake --build . --target install
+#cp bin/Debug/AYT_tutorial.so ../
--- AYT_wd/Tutorial/build/build_all.sh
+++ AYT_wd/Tutorial/build/build_all.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cmake -DCMAKE_BUILD_TYPE=Release -Dboost_path="" CMakeLists.txt
+cmake -DCMAKE_BUILD_TYPE=Release -Dboost_path="C:/Users/Gasta/Documents/Visual Studio 2017/Librerie/boost_1_68_0/" CMakeLists.txt
 cmake --build . --target install
-cp lib/libAYT_tutorial.so ../AYT_tutorial.so
+cp bin/Release/AYT_tutorial.so ../
+#cmake -DCMAKE_BUILD_TYPE=Debug -Dboost_path="C:/Users/Gasta/Documents/Visual Studio 2017/Librerie/boost_1_68_0/" CMakeLists.txt
+#cmake --build . --target install
+#cp bin/Debug/AYT_tutorial.so ../
